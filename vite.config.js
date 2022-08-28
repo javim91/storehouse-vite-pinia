3babb4ec9d9618c07ad2a3a9f73bf10b8afe395b
javim91 <fjmc1991@gmail.com>
2022-08-28 23:37:08 +0100

    Add vite-plugin-rewrite-all

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 export const aliases = {
   '@': resolve(__dirname, './src'),
@@ -8,7 +9,10 @@ export const aliases = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    pluginRewriteAll()
+  ],
   resolve: {
       alias: aliases,
   },
